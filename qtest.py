b28fdb0997631ff56f61972c318a3c763c76e06e
marius <marius@krach.krach>
2015-01-15 16:13:25 +0100

    added files

---
+++ qtest.py
@@ -0,0 +1,18 @@
+from pyrocko.gf import *
+
+# Eine station aus dem webnet
+# KVC
+t = Target(lat=50.2059,
+           lon=12.5152,
+           elevation=580,
+           codes=('', 'KVC', '', 'Z'))
+
+s = Source(lat=50.24,
+           lon=12.45,
+           depth=9.0,
+           magnitude=0.7)
+
+
+
+
+
