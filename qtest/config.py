3da3556b825c67c807c8fba56bdeb0d01dcd1fd6
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-09-30 20:25:19 +0200

    new config

---
+++ qtest/config.py
@@ -0,0 +1,20 @@
+from pyrocko.guts import Object, String, Float, Dict, Tuple, List
+
+class QConfig(Object):
+    want_phase = String.T()
+    earthmodel = String.T()
+    markers = String.T()
+    events = String.T()
+    traces = String.T()
+    stations = String.T()
+    output = String.T()
+    magdiffmax = Float.T(default=0.5)
+    method = String.T(default='mtspec')
+    fmax_lim = Float.T(default=85.)
+    fmin_lim = Float.T(default=40.)
+    fminrange = Float.T(default=30.)
+    snr_min = Float.T(default=1800)
+    cc_min = Float.T(default=0.75)
+    min_magnitude = Float.T(default=1.)
+    max_magnitude =  Float.T(default=4.)
+    whitelist = List.T(Tuple.T(4, String.T()), optional=True)
