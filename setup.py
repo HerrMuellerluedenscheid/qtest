5696d679f9fab28e1490e4cedcdfd10ffe9a479d
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-09-30 20:25:56 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+
+
+import os
+import time
+import shutil
+
+from distutils.core import setup
+
+
+
+
+setup(
+    name='qtest',
+    version='0.0',
+    description='Q',
+    author='Marius Kriegerowski',
+    #package_dir={'': 'src'},
+    packages=['qtest'],
+    #py_modules=['src/q'],
+    #py_modules=['src/utils'],
+)
+
