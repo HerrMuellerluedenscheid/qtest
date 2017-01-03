f07f899f659570ba7cf975ccc2a174e8fcb22bbb
Marius Kriegerowski <marius@gfz-potsdam.de>
2017-01-03 21:16:12 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -8,16 +8,11 @@ import shutil
 from distutils.core import setup
 
 
-
-
 setup(
     name='qtest',
     version='0.0',
     description='Q',
     author='Marius Kriegerowski',
-    #package_dir={'': 'src'},
     packages=['qtest'],
-    #py_modules=['src/q'],
-    #py_modules=['src/utils'],
 )
 
