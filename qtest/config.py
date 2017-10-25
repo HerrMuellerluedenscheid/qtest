8492690f0be393a43a27b1afd450c89c8889ffba
Marius Kriegerowski <kriegero@uni-potsdam.de>
2017-10-25 15:42:01 +0200

    improve config

--- qtest/config.py
+++ qtest/config.py
@@ -1,4 +1,4 @@
-from pyrocko.guts import Object, String, Float, Dict, Tuple, List
+from pyrocko.guts import Object, String, Float, Dict, Tuple, List, Int, Bool
 from pyrocko import model, util
 
 
@@ -9,31 +9,37 @@ class SyntheticTestConfig(Object):
 
 
 class QConfig(Object):
-    type = String.T(default='real', optional=True)
     want_phase = String.T()
     earthmodel = String.T()
     markers = String.T()
     events = String.T()
     traces = String.T()
     stations = String.T()
-    output = String.T()
-    magdiffmax = Float.T(default=0.5)
+    outdir = String.T()
+    channel = String.T()
+    window_length = Float.T()
+    type = String.T(default='real', optional=True)
+    position = Float.T(default=0.9)
+    mag_delta_max = Float.T(default=0.5)
     method = String.T(default='mtspec')
     fmax_lim = Float.T(default=85.)
     fmin_lim = Float.T(default=40.)
     fminrange = Float.T(default=30.)
-    snr_min = Float.T(default=1800)
-    cc_min = Float.T(default=0.75)
-    min_magnitude = Float.T(default=1.)
-    max_magnitude =  Float.T(default=4.)
+    snr = Float.T(default=5., optional=True)
+    cc_min = Float.T(default=None, optional=True)
+    mag_min = Float.T(default=1.)
+    mag_max =  Float.T(default=3.)
+    time_bandwidth = Float.T(default=5.)
+    ntapers = Int.T(default=3)
+    rsquared_min = Float.T(default=None, optional=True)
+    rmse_max = Float.T(default=None, optional=True)
     whitelist = List.T(String.T(), optional=True)
     traversing_distance_min = Float.T(default=1000.)
     traversing_ratio = Float.T(default=2.)
-    file_format = String.T(optional=True)
+    file_format = String.T(optional=True, default='guess')
     synthetic_config = SyntheticTestConfig.T(optional=True)
-    window_length = Float.T(optional=True)
-    channel = String.T()
-    fn_couples = String.T(optional=True)
+    fn_couples = String.T(optional=True, default='/tmp/couples')
+    plot = Bool.T(default=False)
 
     def __init__(self, *args, **kwargs):
         Object.__init__(self, *args, **kwargs)
@@ -53,3 +59,17 @@ class QConfig(Object):
             if s.nsl() in wl:
                 f.append(s)
         return f
+
+    @classmethod
+    def example(cls):
+        c = cls(
+            want_phase='P',
+            earthmodel='/data/models/earthmodel_malek_alexandrakis.nd',
+            markers='/home/marius/josef_dd/hypodd_markers_josef.pf',
+            events='/home/marius/josef_dd/events_from_sebastian_check_M1.pf',
+            traces='/data/webnet/gse2/2008Oct',
+            stations='/data/meta/stations.pf',
+            outdir='qopher.out',
+            channel='SHZ')
+
+        return c
