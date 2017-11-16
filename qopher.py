7bd7872aa3e5acf3baef1356b6f4315aa6f3fd27
Marius Kriegerowski <kriegero@uni-potsdam.de>
2017-11-16 10:18:58 +0100

    upd

--- qopher.py
+++ qopher.py
@@ -1,3 +1,6 @@
+import matplotlib as mpl
+mpl.use('Agg')
+
 import logging
 import numpy as num
 import os
@@ -7,7 +10,7 @@ from pyrocko import model
 from pyrocko import cake
 from pyrocko import pile
 from pyrocko import util
-from pyrocko.gui_util import PhaseMarker, Marker
+from pyrocko.gui.util import PhaseMarker, Marker
 from pyrocko import trace
 import matplotlib.pyplot as plt
 from autogain.autogain import PickPie
@@ -21,12 +24,14 @@ logger = logging.getLogger('qopher')
 
 pjoin = os.path.join
 
-
 class SNRException(Exception):
     pass
 
 
 def get_spectrum(ydata, deltat, config):
+    ''' Return the spectrum on *ydata*, considering the *snr* stored in
+    config
+    '''
     isplit = int(len(ydata) / 2)
     a, f = mtspec(data=ydata[isplit:isplit*2], delta=deltat, number_of_tapers=config.ntapers,
         time_bandwidth=config.time_bandwidth, nfft=trace.nextpow2(isplit)*2)
@@ -44,6 +49,30 @@ def get_spectrum(ydata, deltat, config):
     #  plt.plot(ydata[0:isplit])
     #  plt.show()
     return f, a
+
+
+class Counter:
+    def __init__(self, msg):
+        self.msg = msg
+        self.count = 0
+
+    def __call__(self, info=''):
+        self.count += 1
+        logger.info("%s %s (%i)" % (self.msg, info, self.count))
+
+    def __str__(self):
+        return "%s: %i" % (self.msg, self.count)
+
+
+fail_counter = {}
+fail_counter['no_onset'] = Counter('no onset')
+fail_counter['no_waveforms'] = Counter('no waveform data')
+fail_counter['low_snr'] = Counter('Low SNR')
+fail_counter['cc'] = Counter('Low CC')
+fail_counter['rmse'] = Counter('RMSE')
+fail_counter['IndexError'] = Counter('IndexError')
+fail_counter['IndexError'] = Counter('Low rsquared')
+fail_counter['slope'] = Counter('WARN: slope is NAN')
 
 
 def run_qopher(config):
@@ -97,13 +126,20 @@ def run_qopher(config):
     #     #('', 'LBC', ''),
     #     #('SX', 'WERN', ''),
     # ]
+    if not os.path.isdir(config.outdir):
+        os.mkdir(config.outdir)
+
+    config.dump(filename=pjoin(config.outdir, 'config.yaml'))
 
     phases = [cake.PhaseDef(config.want_phase), cake.PhaseDef(config.want_phase.lower())]
 
     velocity_model = cake.load_model(config.earthmodel)
     events = model.load_events(config.events)
     events = filter(lambda x: config.mag_max>=x.magnitude>=config.mag_min, events)
-
+    if config.tstart:
+        events = filter(lambda x: x.time > util.stt(config.tstart), events)
+    if config.tstop:
+        events = filter(lambda x: x.time < util.stt(config.tstop), events)
     events_by_time = {}
     for e in events:
         events_by_time[e.time] = e
@@ -165,12 +201,10 @@ def run_qopher(config):
     qs = []
     cc = None
 
-    if not os.path.isdir(config.outdir):
-        os.mkdir(config.outdir)
 
     ncandidates = len(candidates)
     for icand, candidate in enumerate(candidates):
-        print '... %1.1f' % ((icand/float(ncandidates)) * 100.)
+        print('... %1.1f' % ((icand/float(ncandidates)) * 100.))
         s1, s2, t, td, pd, totald, i1, travel_time_segment = candidate
 
         for s in [s1, s2]:
@@ -182,7 +216,7 @@ def run_qopher(config):
         onset1 = pie.t(config.want_phase, (s1, t.codes[:3]))
         onset2 = pie.t(config.want_phase, (s2, t.codes[:3]))
         if not onset1 or not onset2:
-            print 'No onset'
+            fail_counter['no_onset']()
             continue
 
         if onset1 < 10000:
@@ -202,7 +236,7 @@ def run_qopher(config):
         tmax_group2 = tmin2 + config.window_length * config.position
         if not data_pile.is_relevant(tmin_group1, tmax_group1, group_selector) or \
                 not data_pile.is_relevant(tmin_group2, tmax_group2, group_selector):
-            print 'No waveforms... skipped'
+            fail_counter['no_waveforms']()
             continue
 
         t_noise1 = (tmax_group1 - tmin_group1) * 2
@@ -218,15 +252,14 @@ def run_qopher(config):
                 tmin=tmin_group2-t_noise2, tmax=tmax_group2))[0][0]
 
         except IndexError as e:
-            print(e)
+            fail_counter['IndexError']()
             continue
 
         if config.cc_min:
             cc = trace.correlate(tr1, tr2, normalization='normal').max()[1]
             if cc < config.cc_min:
-                print 'low cc', cc
+                fail_counter['cc'](cc)
                 continue
-            print 'cc', cc
 
         y1 = tr1.ydata
         y2 = tr2.ydata
@@ -241,11 +274,9 @@ def run_qopher(config):
             f1, a1 = get_spectrum(y1, tr1.deltat, config)
             f2, a2 = get_spectrum(y2, tr2.deltat, config)
         except SNRException as e:
-            print(e)
+            fail_counter['low_snr'](e)
             continue
 
-        if not all(f1 == f2):
-            print('WARN: f1 != f2')
         f = f1
 
         ratio = num.log(num.sqrt(a1)/num.sqrt(a2))
@@ -261,16 +292,16 @@ def run_qopher(config):
             # RMS Error:
             RMSE = num.sqrt(num.sum((ratio[indx]-(intercept+f_selected*slope))**2)/float(len(f_selected)))
             if RMSE > config.rmse_max:
-                print 'XXX  RMSE too large XXXX', RMSE
+                fail_counter['rmse']()
                 continue
 
         if config.rsquared_min:
             if r_value**2 < config.rsquared_min:
-                print 'low rsquared: %s', r_value**2
+                fail_counter['rsquared'](r_value**2)
                 continue
 
         if num.isnan(slope):
-            print "Warning: slope was Nan: skipped"
+            fail_counter['slope']()
             continue
 
         if config.plot:
@@ -295,12 +326,12 @@ def run_qopher(config):
             ratio_selected = ratio[indx]
             slope, intercept, r_value, p_value, std_err = stats.linregress(
                 f[indx], ratio[indx])
-            print slope, 'b', 'rvalue', r_value, 'pvalue', p_value
+            print("slope %s, rvalue %s, pvalue %s" % (slope, r_value, p_value))
 
             axs[3].plot(f_selected, ratio_selected)
             axs[3].plot([config.fmin_lim, config.fmax_lim], [intercept+config.fmin_lim*slope, intercept + config.fmax_lim*slope])
             axs[3].set_title('log(sqrt/sqrt), R=%s, cc=%s' % (r_value, cc))
-            print '>0, <0:',  slope_positive, slope_negative, counter
+            print('>0, <0: %s, %s, counter %s' % slope_positive, slope_negative, counter)
 
             fig.savefig(
                 pjoin(config.outdir, '%s/example_wave_spectra_%s.png' % (tr1.station, int(tr1.tmin))))
@@ -312,17 +343,27 @@ def run_qopher(config):
         # qs.append(slope/ray.t[-1])
         qs.append(slope/travel_time_segment)
         
-        print slope, 'a'
+        print("slope %s" % slope)
 
     print(qs)
+    status_str = ''
+    for k, v in fail_counter.items():
+        status_str += "%s\n" % v
+    
+    with open(pjoin(config.outdir, 'status.txt'), 'w') as f:
+        f.write(status_str)
+        f.write('median 1./q = %s\n' % num.median(qs))
+        f.write('Nsamples = %s\n' % len(qs))
+        # f.write('Npositive = %s\n' % len(qs))
+
+    num.savetxt(pjoin(config.outdir, 'qs_inv.txt'), num.array(qs).T)
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.hist(qs, bins=41)
 
     fig.savefig(pjoin(config.outdir, 'slope_histogram.png'))
-    num.savetxt(pjoin(config.outdir, '%s/qs_inv.txt' % (config.whitelist[0][1]), num.array(qs).T))
-    #model.dump_events(events.values(), 'tobepicked.pf')
-    #plt.show()
+
 
 if __name__ == '__main__':
     from qtest import config
