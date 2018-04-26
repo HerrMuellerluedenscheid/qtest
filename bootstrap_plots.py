e7f3128080851fe36e2c38fcb5ab2dd667f713bc
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2018-04-26 16:13:43 +0200

    wip: add polarity

--- bootstrap_plots.py
+++ bootstrap_plots.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
-
+# encoding=utf8  
 import matplotlib
-# matplotlib.use('Qt5Agg')
+
 matplotlib.use('Agg')
+from pyrocko import orthodrome as ortho
+from pyrocko.gui import marker
 import numpy as num
 import sys
 from collections import defaultdict
 from pyrocko.gf import *
+from pyrocko.model.station import load_stations
+from pyrocko.model.event import Event
 import matplotlib.pyplot as plt
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
-'''{'q': 0.094188365758785114, 'event1': <qtest.sources.DCSourceWid object at
- 0x7fd4c7050be0>, 'td': 1875.4923075715485, 'snr1': 25.671509, 'target':
- <pyrocko.gf.targets.Target object at 0x7fd4ab2414e0>, 'totald':
- 10301.983523752433, 'pd': 340.58347460133115, 'rmse': 0.10328656169950369,
- 'rsquared': 0.77178789450064877, 'snr2': 55.453697, 'incidence':
- 7.4748259117506137, 'event2': <qtest.sources.DCSourceWid object at
- 0x7fd4c723efd0>, 'cc': 0.89884726787060842},
-'''
 
+
+stations = load_stations('/data/meta/stations.pf')
+station_by_nsl = {}
+for s in stations:
+    station_by_nsl[s.nsl()] = s
+station_nkc = station_by_nsl[('', 'NKC','')]
 
 point_size = 0.3
 fn = sys.argv[1]
+
+if len(sys.argv)>2:
+    with open(sys.argv[2], 'rb') as f:
+        slopes_ratios = pickle.load(f, encoding='latin1')
+else:
+    fn_slope_ratios = None 
+    slopes_ratios = {}
+
 
 with open(fn, 'rb') as f:
     stats = pickle.load(f)
@@ -39,6 +49,17 @@ with open(fn, 'rb') as f:
         lateral_distance = num.ndarray(n),
         magnitude_1 = num.ndarray(n),
         magnitude_2 = num.ndarray(n),
+        origin_time_1 = num.ndarray(n),
+        origin_time_2 = num.ndarray(n),
+        azimuth_e1_e2 = num.ndarray(n),
+        azimuth_NKC_1 = num.ndarray(n),
+        azimuth_NKC_2 = num.ndarray(n),
+        azimuth_NKC_delta = num.ndarray(n),
+        northing_1 = num.ndarray(n),
+        northing_2 = num.ndarray(n),
+        slope_ratio_1 = num.ndarray(n),
+        slope_ratio_2 = num.ndarray(n),
+
         #fignames = [],
 
         # waveform measures,
@@ -55,6 +76,11 @@ with open(fn, 'rb') as f:
         incidence = num.ndarray(n),
         cc = num.ndarray(n))
 
+    matrices.update({
+        'polarity_1': [],
+        'polarity_-1': [],
+        'polarity_None': []})
+
     q_by_event_involved = defaultdict(list)
 
     for i, s in enumerate(stats):
@@ -65,6 +91,16 @@ with open(fn, 'rb') as f:
         matrices['lateral_distance'][i] = s['event1'].distance_to(s['event2'])
         matrices['magnitude_1'][i] = s['event1'].magnitude
         matrices['magnitude_2'][i] = s['event2'].magnitude
+        matrices['origin_time_1'][i] = s['event1'].time
+        matrices['origin_time_2'][i] = s['event2'].time
+        matrices['northing_1'][i] = s['event1'].lat
+        matrices['northing_2'][i] = s['event2'].lat
+        matrices['azimuth_e1_e2'][i] = ortho.azimuth(s['event1'], s['event2'])
+        matrices['azimuth_NKC_1'][i] = ortho.azimuth(s['event1'], station_nkc)
+        matrices['azimuth_NKC_2'][i] = ortho.azimuth(s['event2'], station_nkc)
+        matrices['azimuth_NKC_delta'][i] = abs((matrices['azimuth_NKC_1'][-1] - matrices['azimuth_NKC_2'][-1]))
+        matrices['slope_ratio_1'][i] = num.log(slopes_ratios.get(s['event1'].name, 5.))
+        matrices['slope_ratio_2'][i] = num.log(slopes_ratios.get(s['event2'].name, 5.))
         matrices['snr_1'][i] = s['snr1']
         matrices['snr_2'][i] = s['snr2']
 
@@ -92,12 +128,17 @@ with open(fn, 'rb') as f:
         std_by_event.append(num.std(1./num.median(q)))
         nqs.append(len(q))
 
+    print('loading markers from NKC')
+    markers_nks = marker.PhaseMarker.load_markers('/home/marius/josef_dd/markers_with_polarities.pf')
+    emarkers = [marker.EventMarker(ie) for ie in e]
+
+
     ii = num.argsort(qs_by_event)
 
     for i in ii:
-        print('event: %s  Nmeasures: %s  median: %s  std: %s' % (e[i], nqs[i],
-                                                                 qs_by_event[i],
-                                                                 std_by_event[i]))
+        print('event: %s  Nmeasures: %s  median: %s  std: %s  slope_ratio: %s' % (
+            e[i], nqs[i], qs_by_event[i], std_by_event[i],
+            slopes_ratios.get(e[i], None)))
 
     keys = sorted(list(matrices.keys()))
     combinations = []
@@ -123,11 +164,7 @@ with open(fn, 'rb') as f:
             elif jcolumn == len(keys)-1:
                 ax.text(0., 1., str(ki), transform=ax.transAxes)
 
-            #if irow >= jcolumn:
-            #    ax.axis('off')
-            #    continue
             ax.axis('off')
-
             ax.scatter(matrices[ki], matrices[kj], s=point_size, color='black', alpha=0.2)
 
     else:
@@ -148,10 +185,10 @@ with open(fn, 'rb') as f:
             ax = axs[ikey][1]
             ax.scatter(matrices['qs'], matrices[k], s=point_size, color='black', alpha=0.2)
             ax.axvline(0.0, color='black', alpha=0.15)
-            ax.set_xlim(-0.1, 0.1)
+            ax.set_xlim(-0.3, 0.3)
 
 
     fig.subplots_adjust(#hspace=0.02, wspace=0.02, top=0.95,
                         left=0.15, bottom=0.02, right=0.95)
-    fig.savefig('covariances.png', dpi=400)
+    fig.savefig('covariances.png', dpi=260)
     # plt.show()
