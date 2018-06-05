1b1a83d6aec4a59f0b6a6de08712c2d3a40dd41b
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2018-06-05 10:36:44 +0200

    linkage

--- images/cluster_linkage.py
+++ images/cluster_linkage.py
@@ -5,7 +5,8 @@ from scipy.spatial.distance import squareform
 from scipy import stats
 from pyrocko.model import load_events, dump_events
 from matplotlib import pyplot as plt
-from pyrocko import automap, gmtpy
+from pyrocko import automap, gmtpy, gui
+from pyrocko.gui import marker
 
 '''
 Note: Methods centroid, median and ward are correctly
@@ -20,7 +21,7 @@ def flatten(clusters):
 
 
 def make_cluster_map(clusters):
-    
+
     clat = []
     clon = []
     for e in flatten(clusters):
@@ -48,11 +49,13 @@ def make_cluster_matrix(Z, labels):
     fig = plt.figure(figsize=(8, 8))
     ax1 = fig.add_axes([0.05, 0.1, 0.2, 0.6])
 
-    Z1 = dendrogram(Z, orientation='right', labels=event_names)
+    print('L1', labels)
+    Z1 = dendrogram(Z, orientation='right', labels=labels)
     ax1.set_xticks([])
 
     ax2 = fig.add_axes([0.3, 0.71, 0.6, 0.2])
     Z2 = dendrogram(Z)
+    print(Z1)
     ax2.set_xticks([])
     ax2.set_yticks([])
 
@@ -72,30 +75,62 @@ def make_cluster_matrix(Z, labels):
     axcolor = fig.add_axes([0.91, 0.1, 0.02, 0.6])
     plt.colorbar(im, cax=axcolor)
 
+    print('L2', labels)
+    clusters = {}
+    for ename, color in zip(Z1['ivl'], Z1['color_list']):
+        import pdb
+        pdb.set_trace()
+        c = clusters.get(color, [])
+        c.append(marker.EventMarker(names_to_events[ename]))
+        clusters[color] = c
+    print('found %s clusters:' % len(clusters))
+    for icluster, events in clusters.items():
+        print(icluster, 'xxxxxxxx')
+        for e in events:
+            print(e.get_event().name)
+    return clusters.values()
+
 
 def get_clustered_events(Z, events):
-    cut = fcluster(Z, 0.8, 'distance')
+    cut = fcluster(Z, 1.4, 'distance')
     clusters = []
     for i_cluster in num.unique(cut):
         mask = (i_cluster == cut)
         cluster_events = [events[i] for i in num.where(mask)[0]]
         clusters.append(cluster_events)
-
+    print('found %s clusters' % len(clusters))
     return clusters
 
 
-def set_depth_by_cluster(clusters):
+def get_clustered_events_by_dendrogram(Z, events, labels):
+    name_to_events = {e.get_event().name: e for e in events}
+    print('L2', labels)
+    d = dendrogram(Z, no_plot=True, labels=labels)
+    clusters = {}
+    for ename, color in zip(d['ivl'], d['color_list']):
+        c = clusters.get(color, [])
+        c.append(marker.EventMarker(names_to_events[ename]))
+        clusters[color] = c
+    print('found %s clusters:' % len(clusters))
+    for icluster, events in clusters.items():
+        print(icluster, 'xxxxxxxx')
+        for e in events:
+            print(e.get_event().name)
+    return clusters.values()
+
+
+def set_attribute_by_cluster(clusters, attribute):
+    f = 1 if attribute == 'kind' else 1000
     for i, events in enumerate(clusters):
         for e in events:
-            e.depth = (i+1)*1000
+            setattr(e, attribute, (i+1)*f)
 
 
 if __name__ == '__main__':
     normalize = False
     fn = sys.argv[1]
     fn_events = sys.argv[2]
-    
-    
+
     # method = 'weighted'       # works well for cc
     method = 'complete'       # works well for cc
     optimal_ordering = False
@@ -104,13 +139,11 @@ if __name__ == '__main__':
     event_names = [e.name for e in events]
     names_to_events = {e.name: e for e in events}
 
-    labels = []
     data = []
     with open(fn, 'r') as f:
         for line in f.readlines():
             e1, e2, cc = line.split()
             data.append(float(cc))
-            labels.append((e1, e2))
 
     y = num.array(data)
 
@@ -119,7 +152,6 @@ if __name__ == '__main__':
         print('normalize with factor %1.4f' % num.max(y))
 
     print('len(y): %s, min(y): %s, max(y): %s' % (len(y), y.min(), y.max()))
-    print('n labels: %s' % (len(labels)))
     print('mean(y): %1.4f' % num.mean(y))
 
     fig = plt.figure()
@@ -129,12 +161,14 @@ if __name__ == '__main__':
     plt.colorbar(im, cax=axcolor)
 
     Z = linkage(y, method)
-    make_cluster_matrix(Z, labels)
+    make_cluster_matrix(Z, event_names)
 
-    event_clusters = get_clustered_events(Z, events)
-
+    event_markers = [marker.EventMarker(e) for e in events]
+    #  event_clusters = get_clustered_events(Z, event_markers)
+    event_clusters = get_clustered_events_by_dendrogram(Z, event_markers, event_names)
     # make_cluster_map(event_clusters)
-    set_depth_by_cluster(event_clusters)
+    set_attribute_by_cluster(event_clusters, 'kind')
     events_clustered = flatten(event_clusters)
-    dump_events(events_clustered, 'events_hirarchy_clustered.pf')
+    # dump_events(events_clustered, 'events_hirarchy_clustered.pf')
+    gui.marker.save_markers(events_clustered, 'event_markers_clustered.pf')
     plt.show()
