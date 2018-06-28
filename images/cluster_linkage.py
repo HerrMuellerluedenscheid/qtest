2c595bb2b30cbbbf345466a60e2f45087d0ac6c7
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2018-06-28 19:16:20 +0200

    cluster filter

--- images/cluster_linkage.py
+++ images/cluster_linkage.py
@@ -82,25 +82,38 @@ if __name__ == '__main__':
     fn_events = sys.argv[2]
 
     # method = 'average'
-    method = 'single'
-    # method = 'weighted'
-    # method = 'complete'
-    distance = 0.442   # dist_cut for cluster selection
+    # method = 'single'
+    #method = 'weighted'
+    method = 'complete'
+    distance = 0.76   # dist_cut for cluster selection
+    min_cluster_size = 10
 
     events = load_events(fn_events)
     event_names = [e.name for e in events]
     names_to_events = {e.name: e for e in events}
 
     data = []
+    events1 = []
+    events2 = []
     with open(fn, 'r') as f:
         for line in f.readlines():
             e1, e2, cc = line.split()
             data.append(float(cc))
+            events1.append(e1)
+            events2.append(e2)
+
+    lats1 = [names_to_events[e].lat for e in events1]
+    lats2 = [names_to_events[e].lat for e in events2]
+
+    isorting = num.argsort(lats1)
+
+    # y = num.array(data)[isorting]
     y = num.array(data)
 
     print('len(y): %s, min(y): %s, max(y): %s' % (len(y), y.min(), y.max()))
     print('mean(y): %1.4f' % num.mean(y))
 
+    #plot_distance_matrix(y[isorting])
     plot_distance_matrix(y)
     Z = linkage(y, method)
     make_cluster_matrix(Z, event_names, distance)
@@ -108,6 +121,9 @@ if __name__ == '__main__':
     event_markers = [marker.EventMarker(e) for e in events]
     event_clusters = get_clustered_events(Z, event_markers, distance=distance)
     print('found %s clusters (distance=%s)' % (len(event_clusters), distance))
+    event_clusters = [ec for ec in event_clusters if len(ec)>= min_cluster_size]
+    print('found %s clusters with length > min_cluster_size=%s' % (len(event_clusters), min_cluster_size))
+
     set_attribute_by_cluster(event_clusters, 'kind')
     events_clustered = flatten(event_clusters)
     gui.marker.save_markers(events_clustered, 'event_markers_clustered.pf')
