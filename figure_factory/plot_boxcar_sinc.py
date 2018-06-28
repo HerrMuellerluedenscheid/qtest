fa69be2738c121cfa2fb09af8fd6395aa32a694f
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2018-06-28 18:53:30 +0200

    labels

--- figure_factory/plot_boxcar_sinc.py
+++ figure_factory/plot_boxcar_sinc.py
@@ -15,14 +15,19 @@ def corner_frequency(t_rupture, t_displacement):
 if __name__  == '__main__':
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
-    f = num.linspace(1E-3, 100, 10000)
-    M0 = 10.
-    
-    t_displacement = 10
-    for t_rupture in [5,  30, 60]:
 
-        ax.loglog(f, boxcar_amp_spec(f, M0, t_rupture, t_displacement))
-        # ax.plot(f, boxcar_amp_spec(f, M0, t_rupture, t_displacement))
-        # ax.set_yscale('log')
+    f = num.linspace(1E-3, 100, 10000)
+    M0 = 1.
+
+    t_displacement = 10
+    for t_rupture in [1,  10, 100]:
+        ax.loglog(f, boxcar_amp_spec(f, M0, t_rupture, t_displacement), label='factor %s' % t_rupture)
+    ax.set_xlabel('f [Hz]')
+    ax.set_ylabel('Amplitude (normalized)')
+    ax.set_xlim(0, 20)
+    ax.set_ylim(1E-10, 10.)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.legend()
+    fig.savefig('stf_compare.png')
     plt.show()
